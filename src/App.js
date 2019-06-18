6da1c1d032504900d99482a8a80e26f661ba0b9a
Abakar Fils Mahamat <mahamatabakrmht@gmail.com>
2019-06-18 00:37:24 +0000

    tout est ok pour la navigation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 import UserTable from './tables/UserTable'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Crud from './moduleSimple/Crud'
+import Crud from './moduleSimple/Crud';
+import  Header from './layout/layout';
 const App = () => {
 	return (
 		<Router>
 			<div>
+				<Header/>
 				<Route exact path="/" component={Crud} />
+				<Route exact path="/dynamique" component={Crud} />
 			</div>
 		</Router>
 	);
---
+++ src/layout/layout.js
@@ -0,0 +1,36 @@
+import  React from 'react';
+import {Link} from 'react-router-dom';
+class Header extends React.Component {
+
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light  bg-primary">
+                <a className="navbar-brand" href="#">Présentation React</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+
+                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dynamique" href="#">CRUD DYNAMIQUE</Link>
+                        </li>
+
+                        <li className="nav-item">
+                            <Link className="nav-link disabled" href="#">Disabled</Link>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Header;
