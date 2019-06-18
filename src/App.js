31c8a92350e115e1ec9759d851b106c8c52b7a33
Abakar Fils Mahamat <mahamatabakrmht@gmail.com>
2019-06-18 00:40:14 +0000

    commencement de la partie attaque de service

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, Fragment } from 'react'
-import AddUserForm from './forms/AddUserForm'
-import EditUserForm from './forms/EditUserForm'
-import UserTable from './tables/UserTable'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Crud from './moduleSimple/Crud';
 import  Header from './layout/layout';
 const App = () => {
